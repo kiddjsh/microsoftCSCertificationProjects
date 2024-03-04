6f7eecc6cce948c5b9362b2b14d212370fd82aea
kiddjsh <joshua.kidder@gmail.com>
2024-03-04 02:08:07 -0500

    initial commit

---
+++ codeChallengeOne/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// dotnet --version 8.0.201
+
+/***Code challenge: Implement a method of the Math class that returns the larger of two numbers***/
+
+/***Find a method of the System.Math class that returns the larger of two numbers.
+
+    You can use either Visual Studio Code "Intellisense" or learn.microsoft.com to 
+    find the method and determine how to call it properly.
+
+    Use the method that you found to assign a value to the variable named largerValue.
+
+    You should be able to pass the two integer values, firstValue and secondValue, to 
+    the method that you found. The method should return a value of type int that 
+    represents the larger of the two arguments that you passed it.
+
+    You can assign the return value to largerValue on a new code line or on the existing 
+    code line that's used to declare largerValue.
+    
+    Use the following dotnet commands to build and run your application: 
+    
+        dotnet build
+        dotnet run
+***/
+
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue;
+largerValue = Math.Max(firstValue, secondValue);
+Console.WriteLine(largerValue);
---
+++ simulatingDiceRoll/Program.cs
@@ -0,0 +1,33 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// dotnet --version 8.0.201
+
+/***This code simulates a dice roll using the Random.Next() method to generate a number, 
+    and the Console.WriteLine() method to display the value.
+   
+    Use the following dotnet commands to build and run your application: 
+    
+        dotnet build
+        dotnet run
+***/
+
+//Random dice = new Random();
+//int roll = dice.Next(1, 7);
+//Console.WriteLine(roll);
+//Console.WriteLine(dice.Next(1, 7));
+
+//int number = 7;
+//string text = "seven";
+//Console.WriteLine(number);
+//Console.WriteLine();
+//Console.WriteLine(text);
+
+Random dice = new Random();
+int roll1 = dice.Next();
+int roll2 = dice.Next(101);
+int roll3 = dice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
+
