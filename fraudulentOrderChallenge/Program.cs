06c366b1b8170c7c4ecc317c341952479354a164
kiddjsh <joshua.kidder@gmail.com>
2024-03-06 15:29:04 -0500

    initial commit

---
+++ fraudulentOrderChallenge/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// dotnet --version 8.0.201
+
+/***Use the following dotnet command to create your new folder directory:
+
+        dotnet new console -o ./nameOfFolder/
+
+    Use the following dotnet commands to build and run your application: 
+    
+        dotnet build
+        dotnet run
+***/
+
+/*
+Your team has found a pattern. Orders that start with the letter "B" encounter fraud at a rate 25 times greater than the normal rate. 
+You write new code that outputs the Order ID of new orders where the Order ID starts with the letter "B". This will be used by the 
+fraud team to investigate further.
+*/
+
+string[] orderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string orderID in orderIDs)
+{
+    if (orderID.StartsWith("B"))
+    {
+        Console.WriteLine(orderID);
+    }
+}
---
+++ inventoryReportingProject/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// dotnet --version 8.0.201
+
+/***Use the following dotnet command to create your new folder directory:
+
+        dotnet new console -o ./nameOfFolder/
+
+    Use the following dotnet commands to build and run your application: 
+    
+        dotnet build
+        dotnet run
+***/
+
+/*
+creates and initializes an array that stores the number of 
+finished products in each bin
+*/
+int[] inventory = { 200, 450, 700, 175, 250 };
+
+//sums the value of each element in the array
+int sum = 0;
+//stores the number of the bin whose inventory is currently being processed
+int bin = 0;
+
+/*
+added foreach to iterate through array that temporarily assigns 
+the value of the current array element to an int variable named 
+items
+*/
+foreach (int items in inventory)
+{
+    //adds the current value stored in items to the sum variable
+    sum += items;
+    //increments the value of the variable by 1
+    bin++;
+    //reports the current state of the inventory using the counter variable bin, the temporary foreach variable items, and the sum variable
+    Console.WriteLine($"Bin {bin} = {items} items (Running total: {sum})");
+}
+//reports the final sum of items in the inventory
+Console.WriteLine($"We have {sum} items in inventory.");
