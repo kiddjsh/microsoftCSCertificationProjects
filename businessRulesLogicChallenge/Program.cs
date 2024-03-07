66cd6c2fe77664888f9cf7a512c10206c34b55b5
kiddjsh <joshua.kidder@gmail.com>
2024-03-07 01:10:16 -0500

    initial commit

---
+++ businessRulesLogicChallenge/Program.cs
@@ -0,0 +1,51 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// dotnet --version 8.0.201
+
+/*Use the following dotnet command to create your new folder directory:
+
+        dotnet new console -o ./nameOfFolder/
+
+    Use the following dotnet commands to build and run your application: 
+    
+        dotnet build
+        dotnet run
+*/
+
+/*
+This code logic implements decision logic based on a series of business rules. 
+The business rules specify the access that will be granted to users based on 
+their role-based permissions and their career level. Code branches will 
+display a different message to the user depending on their permissions and 
+level.
+*/
+
+string permission = "Admin|Manager";
+int level = 53;
+
+if (permission.Contains("Admin"))
+{
+    if (level > 55)
+    {
+        Console.WriteLine("Welcome, Super Admin user.");
+    }
+    else
+    {
+        Console.WriteLine("Welcome, Admin user.");
+    }
+}
+else if (permission.Contains("Manager"))
+{
+    if (level >= 20)
+    {
+        Console.WriteLine("Contact an Admin for access.");
+    }
+    else
+    {
+        Console.WriteLine("You do not have sufficient privileges.");
+    }
+}
+else
+{
+    Console.WriteLine("You do not have sufficient privileges.");
+}
