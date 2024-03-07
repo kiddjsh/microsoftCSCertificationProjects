ab7caea11c69150e1b6415a250cccbf5d2466767
kiddjsh <joshua.kidder@gmail.com>
2024-03-07 13:44:44 -0500

    updated program.cs

--- problematicCodeChallenge/Program.cs
+++ problematicCodeChallenge/Program.cs
@@ -11,3 +11,32 @@
         dotnet build
         dotnet run
 */
+
+/*
+This variable scope challenge is to update code blocks and variable scopes to fix a poorly written code sample.
+Complete code updates required in adjusting variable scopes as needed, and with Console.WriteLine() statements 
+to produce the desired output, while also optimizing the code for readability. You should have around 17 lines
+of code depending on condensing whitespace and other factors.
+
+The biggest changes to the problematic code included:
+
+    Moving the declaration of the total and found variables outside of the foreach statement.
+    Initializing both the total and found variables with sensible default values.
+    Removing the code blocks (curly braces) from the if statements.
+*/
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+foreach (int number in numbers)
+{
+    total += number;
+    if (number == 42)
+        found = true;
+}
+
+if (found)
+    Console.WriteLine("Set contains 42");
+
+Console.WriteLine($"Total: {total}");
