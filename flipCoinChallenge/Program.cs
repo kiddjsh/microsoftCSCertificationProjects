f02da34e9252464928fffa4726e25f9ac400284b
kiddjsh <joshua.kidder@gmail.com>
2024-03-07 01:02:09 -0500

    initial commit

---
+++ flipCoinChallenge/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// dotnet --version 8.0.201
+
+/*Use the following dotnet command to create your new folder directory:
+
+        dotnet new console -o ./nameOfFolder/
+
+    Use the following dotnet commands to build and run your application: 
+    
+        dotnet build
+        dotnet run
+*/
+
+/*
+This code logic implement a conditional operator to simulate a "coin flip". 
+The resulting decision logic will display either heads or tails.
+*/
+
+Random coin = new Random();
+int flip = coin.Next(0, 2);
+Console.WriteLine((flip == 0) ? "heads" : "tails");
