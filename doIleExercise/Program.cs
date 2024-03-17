5359ed18e7e3f0490edbe7fb4ed5c1fb8a3cf463
kiddjsh <joshua.kidder@gmail.com>
2024-03-17 00:31:31 -0400

    updated program.cs

---
+++ doIleExercise/Program.cs
@@ -0,0 +1,76 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// dotnet --version 8.0.203
+
+/***Use the following dotnet command to create your new folder directory:
+
+        dotnet new console -o ./nameOfFolder/
+
+    Use the following dotnet commands to build and run your application: 
+    
+        dotnet build
+        dotnet run
+***/
+
+
+/**
+do
+{
+    // This code executes at least one time
+} while (true);
+**/
+
+
+/**
+Random random = new Random();
+int current = 0;
+
+do
+{
+    current = random.Next(1, 11);
+    Console.WriteLine(current);
+} while (current != 7);
+**/
+
+
+/**
+Random random = new Random();
+int current = random.Next(1, 11);
+
+
+//do
+//{
+    //current = random.Next(1, 11);
+    //Console.WriteLine(current);
+//} while (current != 7);
+
+
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
+**/
+
+
+Random random = new Random();
+int current = random.Next(1, 11);
+
+do
+{
+    current = random.Next(1, 11);
+
+    if (current >= 8) continue;
+
+    Console.WriteLine(current);
+} while (current != 7);
+
+/*
+while (current >= 3)
+{
+    Console.WriteLine(current);
+    current = random.Next(1, 11);
+}
+Console.WriteLine($"Last number: {current}");
+*/
