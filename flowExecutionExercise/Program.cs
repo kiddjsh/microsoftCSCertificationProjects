7b449cca587bc0ad85c2f53416318078ac18c6dd
kiddjsh <joshua.kidder@gmail.com>
2024-03-16 23:50:39 -0400

    updated program.cs

--- flowExecutionExercise/Program.cs
+++ flowExecutionExercise/Program.cs
@@ -1,2 +1,84 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// dotnet --version 8.0.203
+
+/***Use the following dotnet command to create your new folder directory:
+
+        dotnet new console -o ./nameOfFolder/
+
+    Use the following dotnet commands to build and run your application: 
+    
+        dotnet build
+        dotnet run
+***/
+
+
+/**
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+}
+
+
+/**
+for (int i = 10; i >= 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+
+/**
+for (int i = 0; i < 10; i += 3)
+{
+    Console.WriteLine(i);
+}
+
+
+/**
+for (int i = 0; i < 10; i++)
+{
+    Console.WriteLine(i);
+    if (i == 7) break;
+}
+
+
+/**
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = names.Length - 1; i >= 0; i--)
+{
+    Console.WriteLine(names[i]);
+}
+
+
+/**
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+foreach (var name in names)
+{
+    // Can't do this:
+    if (name == "David") name = "Sammy";
+}
+
+
+/**
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+for (int i = 0; i < names.Length; i++)
+    if (names[i] == "David") names[i] = "Sammy";
+
+foreach (var name in names) Console.WriteLine(name);
+**/
+
+
+string[] names = { "Alex", "Eddie", "David", "Michael" };
+
+for (int i = 0; i < names.Length; i++)
+{
+    if (names[i] == "David")
+    {
+        names[i] = "Sammy";
+    }
+}
+
+foreach (var name in names)
+{
+    Console.WriteLine(name);
+}
