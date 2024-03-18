6e7c2508acbd03ea3cbe06515a6bc20ce195ea8b
kiddjsh <joshua.kidder@gmail.com>
2024-03-17 23:26:38 -0400

    updated program.cs

--- dataParseExercise/Program.cs
+++ dataParseExercise/Program.cs
@@ -11,3 +11,35 @@
         dotnet build
         dotnet run
 ***/
+
+/**
+
+//--Uses TryParse() to convert a string into an int--//
+string value = "102";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+   Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+   Console.WriteLine("Unable to report the measurement.");
+}
+Console.WriteLine($"Measurement (w/ offset): {50 + result}");
+**/
+
+
+// Modifies the string variable to a value that can't be parsed
+string value = "bad";
+int result = 0;
+if (int.TryParse(value, out result))
+{
+   Console.WriteLine($"Measurement: {result}");
+}
+else
+{
+   Console.WriteLine("Unable to report the measurement.");
+}
+
+if (result > 0)
+   Console.WriteLine($"Measurement (w/ offset): {50 + result}");
