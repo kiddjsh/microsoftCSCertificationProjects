7df57bb328bfd234ffab31da916712e7551f062e
kiddjsh <joshua.kidder@gmail.com>
2024-03-17 20:28:32 -0400

    updated program.cs

--- minMaxValueExercise/Program.cs
+++ minMaxValueExercise/Program.cs
@@ -13,4 +13,10 @@
 ***/
 
 
+Console.WriteLine("");
+Console.WriteLine("Unsigned integral types:");
 
+Console.WriteLine($"byte   : {byte.MinValue} to {byte.MaxValue}");
+Console.WriteLine($"ushort : {ushort.MinValue} to {ushort.MaxValue}");
+Console.WriteLine($"uint   : {uint.MinValue} to {uint.MaxValue}");
+Console.WriteLine($"ulong  : {ulong.MinValue} to {ulong.MaxValue}");
