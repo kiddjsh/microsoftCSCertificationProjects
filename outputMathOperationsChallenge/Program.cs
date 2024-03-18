f2f650450e99bfb6af0bbabf13dde4051a6a12dd
kiddjsh <joshua.kidder@gmail.com>
2024-03-18 00:36:44 -0400

    updated program.cs

---
+++ outputMathOperationsChallenge/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// dotnet --version 8.0.203
+
+/***Use the following dotnet command to create your new folder directory:
+
+        dotnet new console -o ./nameOfFolder/
+
+    Use the following dotnet commands to build and run your application: 
+    
+        dotnet build
+        dotnet run
+***/
+
+
+int value1 = 12;
+decimal value2 = 6.2m;
+float value3 = 4.3f;
+
+
+int result1 = Convert.ToInt32(value1 / value2);
+Console.WriteLine($"Divide value1 by value2, display the result as an int: {result1}");
+
+decimal result2 = value2 / (decimal)value3;
+Console.WriteLine($"Divide value2 by value3, display the result as a decimal: {result2}");
+
+float result3 = value3 / value1;
+Console.WriteLine($"Divide value3 by value1, display the result as a float: {result3}");
