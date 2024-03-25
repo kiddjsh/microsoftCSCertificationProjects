992ec1ab245cc06a8670c51fc8da20c688d805cc
kiddjsh <joshua.kidder@gmail.com>
2024-03-25 19:07:20 -0400

    updated program.cs

--- totalPurchasePriceExercise/Program.cs
+++ totalPurchasePriceExercise/Program.cs
@@ -12,3 +12,42 @@
         dotnet build
         dotnet run
 ***/
+
+
+/*The Contoso Shopping Center is having a super sale! Many items have discounted price. 
+  You're given a list of item prices and a list of corresponding discounts. The discounts 
+  are represented by percentages, for example 50% = 0.5. If a customer spends more than 
+  $30.00, they recieve $5.00 of their total purchase. In this task, you'll write code to 
+  calculate the customer's total. 
+*/
+
+
+double total = 0;
+double minimumSpend = 30.00;
+
+double[] items = {15.97, 3.50, 12.25, 22.99, 10.98};
+double[] discounts = {0.30, 0.00, 0.10, 0.20, 0.50};
+
+for (int i = 0; i < items.Length; i++)
+{
+    total += GetDiscountedPrice(i);
+}
+
+total -= TotalMeetsMinimum() ? 5.00 : 0.00;
+
+Console.WriteLine($"Total: ${FormatDecimal(total)}");
+
+double GetDiscountedPrice(int itemIndex)
+{
+    return items[itemIndex] * (1 - discounts[itemIndex]);
+}
+
+bool TotalMeetsMinimum()
+{
+    return total >= minimumSpend;
+}
+
+string FormatDecimal(double input)
+{
+    return input.ToString().Substring(0, 5);
+}
