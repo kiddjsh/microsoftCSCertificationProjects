a72f8e7058ad1ace0797a56e0d23bdf37462fde8
kiddjsh <joshua.kidder@gmail.com>
2024-03-25 01:04:48 -0400

    updated program.cs

--- syntaxMethodExercise/Program.cs
+++ syntaxMethodExercise/Program.cs
@@ -12,3 +12,37 @@
         dotnet build
         dotnet run
 ***/
+
+
+//calling a method
+int[] a = {1,2,3,4,5};
+
+Console.WriteLine("Contents of Array:");
+PrintArray();
+
+void PrintArray()
+{
+    foreach (int x in a)
+    {
+        Console.Write($"{x} ");
+    }
+    Console.WriteLine();
+}
+
+
+
+//method execution
+Console.WriteLine("Before calling a method, create the world");
+SayHello();
+Console.WriteLine("After calling a method, destroy the world");
+
+void SayHello() 
+{
+    Console.WriteLine("Say Hello!");
+}
+
+
+
+//best practices
+void ShowData(string a, int b, int c);
+void DisplayDate(string month, int day, int year);
