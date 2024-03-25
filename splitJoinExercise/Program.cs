2e0b7da1bc5ae9f8d1c964235ba5cf01a7d28e8f
kiddjsh <joshua.kidder@gmail.com>
2024-03-24 20:14:36 -0400

    updated program.cs

--- splitJoinExercise/Program.cs
+++ splitJoinExercise/Program.cs
@@ -12,3 +12,18 @@
         dotnet build
         dotnet run
 ***/
+
+
+
+string value = "abc123";
+char[] valueArray = value.ToCharArray();
+Array.Reverse(valueArray);
+// string result = new string(valueArray);
+string result = String.Join(",", valueArray);
+Console.WriteLine(result);
+
+string[] items = result.Split(',');
+foreach (string item in items)
+{
+    Console.WriteLine(item);
+}
