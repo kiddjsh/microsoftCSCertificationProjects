dbeb05c45ec01495f89922d7fc8dd07723255100
kiddjsh <joshua.kidder@gmail.com>
2024-03-24 21:44:59 -0400

    updated program.cs

--- formLetterChallenge/Program.cs
+++ formLetterChallenge/Program.cs
@@ -11,3 +11,46 @@
         dotnet build
         dotnet run
 ***/
+
+/*To the sales and marketing company's newest investment products, you send thousands 
+  of personalized letters to the company's existing clients. Your job is to write C# 
+  code to merge personalized information about the customer. The letter contains 
+  information like their existing portfolio and compares their current returns to 
+  projected returns if they were to invest in using the new products.
+
+  Use your new found knowledge of string formatting to build an application that can 
+  merge and format the appropriate content given the previous example output. Pay 
+  particular attention to the white space and make sure you accurately represent this 
+  exact format using C#.
+*/
+
+string customerName = "Ms. Barros";
+
+string currentProduct = "Magic Yield";
+int currentShares = 2975000;
+decimal currentReturn = 0.1275m;
+decimal currentProfit = 55000000.0m;
+
+string newProduct = "Glorious Future";
+decimal newReturn = 0.13125m;
+decimal newProfit = 63000000.0m;
+
+Console.WriteLine($"Dear {customerName},");
+Console.WriteLine($"As a customer of our {currentProduct} offering we are excited to tell you about a new financial product that would dramatically increase your return.\n");
+Console.WriteLine($"Currently, you own {currentShares:N} shares at a return of {currentReturn:P}.\n");
+Console.WriteLine($"Our new product, {newProduct} offers a return of {newReturn:P}.  Given your current volume, your potential profit would be {newProfit:C}.\n");
+
+Console.WriteLine("Here's a quick comparison:\n");
+
+string comparisonMessage = "";
+
+comparisonMessage = currentProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", currentReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", currentProfit).PadRight(20);
+
+comparisonMessage += "\n";
+comparisonMessage += newProduct.PadRight(20);
+comparisonMessage += String.Format("{0:P}", newReturn).PadRight(10);
+comparisonMessage += String.Format("{0:C}", newProfit).PadRight(20);
+
+Console.WriteLine(comparisonMessage);
