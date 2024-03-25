b1e84e333658769000f24dd56411634c75ee91f8
kiddjsh <joshua.kidder@gmail.com>
2024-03-25 02:24:11 -0400

    initial commit

---
+++ methodScopeExercise/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+// dotnet --version 8.0.203
+
+/***Use the following dotnet command to create your new folder directory:
+
+        dotnet new console -o ./nameOfFolder/
+
+    Use the following dotnet commands to build and run your application: 
+    
+        dotnet build
+        dotnet run
+***/
+
+
+
