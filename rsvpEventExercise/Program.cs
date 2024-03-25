fdf50d4b91c0a6e17c81d7f48fdb9fbfa5823953
kiddjsh <joshua.kidder@gmail.com>
2024-03-25 18:41:36 -0400

    updated program.cs

--- rsvpEventExercise/Program.cs
+++ rsvpEventExercise/Program.cs
@@ -13,4 +13,60 @@
         dotnet run
 ***/
 
+string[] guestList = {"Rebecca", "Nadia", "Noor", "Jonte"};
+string[] rsvps = new string[10];
+int count = 0;
 
+//RSVP("Rebecca", 1, "none", true);
+//RSVP("Nadia", 2, "Nuts", true);
+//RSVP("Linh", 2, "none", false);
+//RSVP(name: "Linh", partySize: 2, allergies: "none", inviteOnly: false);
+//RSVP("Tony", 1, "Jackfruit", true);
+//RSVP("Tony", inviteOnly: true, allergies: "Jackfruit",  partySize: 1);
+//RSVP("Noor", 4, "none", false);
+//RSVP("Jonte", 2, "Stone fruit", false);
+RSVP("Rebecca");
+RSVP("Nadia", 2, "Nuts");
+RSVP(name: "Linh", partySize: 2, inviteOnly: false);
+RSVP("Tony", allergies: "Jackfruit", inviteOnly: true);
+RSVP("Noor", 4, inviteOnly: false);
+RSVP("Jonte", 2, "Stone fruit", false);
+ShowRSVPs();
+
+//void RSVP(string name, int partySize, string allergies, bool inviteOnly) 
+void RSVP(string name, int partySize = 1, string allergies = "none", bool inviteOnly = true)
+{
+    if (inviteOnly)
+    {
+        // search guestList before adding rsvp
+    }
+
+    rsvps[count] = $"Name: {name}, \tParty Size: {partySize}, \tAllergies: {allergies}";
+    count++;
+
+    if (inviteOnly)
+    {
+        bool found = false;
+        foreach (string guest in guestList)
+        {
+            if (guest.Equals(name)) {
+                found = true;
+                break;
+            }
+        }
+        if (!found)
+        {
+            Console.WriteLine($"Sorry, {name} is not on the guest list");
+            return;
+        }
+    }
+}
+
+void ShowRSVPs()
+{
+    Console.WriteLine("\nTotal RSVPs:");
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine(rsvps[i]);
+    }
+}
