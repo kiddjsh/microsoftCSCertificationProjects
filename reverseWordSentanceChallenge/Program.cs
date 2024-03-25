1261cb3ee6b3319367719fc603a8c6542d64c6be
kiddjsh <joshua.kidder@gmail.com>
2024-03-24 20:24:27 -0400

    updated program.cs

--- reverseWordSentanceChallenge/Program.cs
+++ reverseWordSentanceChallenge/Program.cs
@@ -11,3 +11,30 @@
         dotnet build
         dotnet run
 ***/
+
+/*Write the code necessary to reverse the letters of each word in place 
+  and display the result. In other words, don't just reverse every 
+  letter in the variable pangram. Instead, you'll need to reverse just 
+  the letters in each word, but print the reversed word in its original 
+  position in the message.
+*/
+
+string pangram = "The quick brown fox jumps over the lazy dog";
+
+// Step 1
+string[] message = pangram.Split(' ');
+
+//Step 2
+string[] newMessage = new string[message.Length];
+
+// Step 3
+for (int i = 0; i < message.Length; i++)
+{
+    char[] letters = message[i].ToCharArray();
+    Array.Reverse(letters);
+    newMessage[i] = new string(letters);
+}
+
+//Step 4
+string result = String.Join(" ", newMessage);
+Console.WriteLine(result);
