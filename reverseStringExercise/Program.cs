00df2ab20a2b2faf7aa41b79c1acf20edcc61316
kiddjsh <joshua.kidder@gmail.com>
2024-03-25 22:57:39 -0400

    updated program.cs

--- reverseStringExercise/Program.cs
+++ reverseStringExercise/Program.cs
@@ -13,3 +13,35 @@
         dotnet run
 ***/
 
+
+/*You may often find that you need to write a method that returns a string. For example, you may 
+  want to retrieve a string from a set of data or modify a string in some way. Write a method to 
+  reverse a string without using string.Reverse. Reverse a string by iterating from the end of 
+  the string; You can use a temporary string to store each letter from end to beginning. 
+*/
+
+string input = "there are snakes at the zoo";
+
+Console.WriteLine(input);
+Console.WriteLine(ReverseSentence(input));
+
+string ReverseSentence(string input) 
+{
+    string result = "";
+    string[] words = input.Split(" ");
+    foreach(string word in words) 
+    {
+        result += ReverseWord(word) + " ";
+    }
+    return result.Trim();
+}
+
+string ReverseWord(string word) 
+{
+    string result = "";
+    for (int i = word.Length - 1; i >= 0; i--) 
+    {
+        result += word[i];
+    }
+    return result;
+}
