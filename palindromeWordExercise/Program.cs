e9cbb7cefe840384efc2f306aed060e2630244ef
kiddjsh <joshua.kidder@gmail.com>
2024-03-25 23:09:36 -0400

    updated program.cs

--- palindromeWordExercise/Program.cs
+++ palindromeWordExercise/Program.cs
@@ -11,3 +11,39 @@
         dotnet build
         dotnet run
 ***/
+
+
+/*Methods with Boolean return types can be simple but are useful in consolidating code. Methods that 
+  return bool values can be called to evaluate data input anywhere, in if statements, in variable 
+  declarations, and more. 
+
+  Write a program to check if several words are a palindrome. A word is a palindrome if it reads the 
+  same backwards and forwards. For example, the word racecar is a palindrome.
+*/
+
+
+string[] words = {"racecar" ,"talented", "deified", "tent", "tenet"};
+
+Console.WriteLine("Is it a palindrome?");
+foreach (string word in words) 
+{
+    Console.WriteLine($"{word}: {IsPalindrome(word)}");
+}
+
+bool IsPalindrome(string word) 
+{
+    int start = 0;
+    int end = word.Length - 1;
+
+    while (start < end) 
+    {
+        if (word[start] != word[end]) 
+        {
+            return false;
+        }
+        start++;
+        end--;
+    }
+
+    return true;
+}
