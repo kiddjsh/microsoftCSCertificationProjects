cb2e0c9acaeee7f1f35fdf46142d441558421067
kiddjsh <joshua.kidder@gmail.com>
2024-03-25 22:49:32 -0400

    updated programs.cs

--- currencyConverterExercise/Program.cs
+++ currencyConverterExercise/Program.cs
@@ -12,3 +12,28 @@
         dotnet build
         dotnet run
 ***/
+
+
+/*You're visiting Vietnam and want to create a brief program that converts currency. You can 
+  assume the current exchange rate is 1 USD = 23500 VND. Write a method that converts USD to 
+  VND.
+*/
+
+
+double usd = 23.73;
+int vnd = UsdToVnd(usd);
+
+Console.WriteLine($"${usd} USD = ${vnd} VND");
+Console.WriteLine($"${vnd} VND = ${VndToUsd(vnd)} USD");
+
+int UsdToVnd(double usd) 
+{
+    int rate = 23500;
+    return (int) (rate * usd);
+}
+
+double VndToUsd(int vnd) 
+{
+    double rate = 23500;
+    return vnd / rate;
+}
